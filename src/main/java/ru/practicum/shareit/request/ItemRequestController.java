fe85bbc652b612067cccf236bed58214d2a73170
Мария <mashunia0306@yandex.ru>
2023-10-27 23:08:18 +0300

    add-bookings

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.request;
 
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
