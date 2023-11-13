94b8dd4d27afe8d06ea95bd19a1649d8344b1895
Мария <mashunia0306@yandex.ru>
2023-11-13 14:38:17 +0300

    add-item-requests

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.exception;
 
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,13 +10,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.Objects;
 
 @RestControllerAdvice
-@Slf4j
 public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         String error = Objects.requireNonNull(e.getFieldError()).getDefaultMessage();
-        log.error(error);
         return new ErrorResponse(error);
     }
 
@@ -27,7 +24,6 @@ public class ErrorHandler {
             ItemNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleBadRequest(RuntimeException e) {
-        log.error(e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
@@ -35,28 +31,24 @@ public class ErrorHandler {
             ItemRequestNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(RuntimeException e) {
-        log.error(e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler({UserAlreadyExistsException.class})
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleAlreadyExistsException(RuntimeException e) {
-        log.error(e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler({ItemOwnerException.class})
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ErrorResponse handleWrongItemOwnerException(RuntimeException e) {
-        log.error(e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleInternalError(Throwable e) {
-        log.error(e.getMessage());
         return new ErrorResponse("Произошла непредвиденная ошибка.");
     }
 
