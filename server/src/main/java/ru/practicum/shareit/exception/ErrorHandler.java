17da8bd0d92521f71966df7208c9bed40aaf034d
Мария <mashunia0306@yandex.ru>
2023-11-23 21:35:27 +0300

    add-docker

--- server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -11,49 +11,49 @@ import java.util.Objects;
 
 @RestControllerAdvice
 public class ErrorHandler {
-//    @ExceptionHandler
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-//        String error = Objects.requireNonNull(e.getFieldError()).getDefaultMessage();
-//        return new ErrorResponse(error);
-//    }
-//
-//    @ExceptionHandler({ValidationException.class, ItemUnavailableException.class,
-//            BookingTimeException.class, BookingStateException.class, BookingStatusException.class,
-//            IllegalArgumentException.class, CommentaryEmptyException.class, ItemRequestExistsException.class,
-//            ItemNotValidException.class})
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    public ErrorResponse handleBadRequest(RuntimeException e) {
-//        return new ErrorResponse(e.getMessage());
-//    }
-//
-//    @ExceptionHandler({UserNotFoundException.class, ItemNotFoundException.class, BookingNotFoundException.class,
-//            ItemRequestNotFoundException.class})
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    public ErrorResponse handleNotFoundException(RuntimeException e) {
-//        return new ErrorResponse(e.getMessage());
-//    }
-//
-//    @ExceptionHandler({UserAlreadyExistsException.class})
-//    @ResponseStatus(HttpStatus.CONFLICT)
-//    public ErrorResponse handleAlreadyExistsException(RuntimeException e) {
-//        return new ErrorResponse(e.getMessage());
-//    }
-//
-//    @ExceptionHandler({ItemOwnerException.class})
-//    @ResponseStatus(HttpStatus.FORBIDDEN)
-//    public ErrorResponse handleWrongItemOwnerException(RuntimeException e) {
-//        return new ErrorResponse(e.getMessage());
-//    }
-//
-//    @ExceptionHandler
-//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//    public ErrorResponse handleInternalError(Throwable e) {
-//        return new ErrorResponse("Произошла непредвиденная ошибка.");
-//    }
-//
-//    @Data
-//    public static class ErrorResponse {
-//        private final String error;
-//    }
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String error = Objects.requireNonNull(e.getFieldError()).getDefaultMessage();
+        return new ErrorResponse(error);
+    }
+
+    @ExceptionHandler({ValidationException.class, ItemUnavailableException.class,
+            BookingTimeException.class, BookingStateException.class, BookingStatusException.class,
+            IllegalArgumentException.class, CommentaryEmptyException.class, ItemRequestExistsException.class,
+            ItemNotValidException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBadRequest(RuntimeException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler({UserNotFoundException.class, ItemNotFoundException.class, BookingNotFoundException.class,
+            ItemRequestNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(RuntimeException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler({UserAlreadyExistsException.class})
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleAlreadyExistsException(RuntimeException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler({ItemOwnerException.class})
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorResponse handleWrongItemOwnerException(RuntimeException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleInternalError(Throwable e) {
+        return new ErrorResponse("Произошла непредвиденная ошибка.");
+    }
+
+    @Data
+    public static class ErrorResponse {
+        private final String error;
+    }
 }
